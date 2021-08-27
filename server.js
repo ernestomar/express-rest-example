8d64282d9d2523474b2ca95f2513756e864a5c58
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2021-08-26 20:49:43 -0400

    Colocando parche de CORS

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ var jsonParser = bodyParser.json()
 
 var app = express();
 
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        res.status(200).json({});
+    }
+    next();
+});
+
 app.get("/", (req, res, next) => {
+    // res.set('Access-Control-Allow-Origin', '*'); 
     res.send("Server running");
 });
 
